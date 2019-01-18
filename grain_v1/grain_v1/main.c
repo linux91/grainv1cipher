eb8793d97303345058dbe36d60ecec288a18a3bf
libohan <libohan91@163.com>
2019-01-19 00:12:34 +0800

    clone the grainv1cipher, then create a xcode project "grain_v1" in its directory, and commit the main.c

---
+++ grain_v1/grain_v1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  grain_v1
+//
+//  Created by bh on 2019/1/19.
+//  Copyright © 2019 bh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
